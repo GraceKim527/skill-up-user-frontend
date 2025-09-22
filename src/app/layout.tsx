0fd1a0c67eb5fef5716c6a4f1115237225980f81
gracekim527 <k07173027@gmail.com>
2025-09-22 10:24:18 +0900

    :lipstick: Style: 프리텐다드 폰트 적용 (#1)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,23 @@
-import './styles/global.css'
-import { ReactNode } from 'react'
+import "./styles/global.css";
+import { ReactNode } from "react";
 
 export const metadata = {
-  title: 'Skill Up Front',
-  description: 'Skill up with Next.js',
-}
+  title: "SkillUp User",
+  description: "스킬업 유저 웹페이지",
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <head />
+      <head>
+        <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin="anonymous"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css"
+        />
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
