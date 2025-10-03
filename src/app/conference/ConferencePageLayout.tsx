cf15e186128fe817deb9c149939fe847b2dc3292
gracekim527 <k07173027@gmail.com>
2025-10-03 23:13:34 +0900

    :lipstick: Style: 이벤트 내 행사 카드 UI 구현 (#4)

--- src/app/conference/ConferencePageLayout.tsx
+++ src/app/conference/ConferencePageLayout.tsx
@@ -1,11 +1,25 @@
 // src/app/conference/ConferencePageLayout.tsx
 
 import EventHeader from "@/components/events/EventHeader";
+import EventCard from "@/components/events/EventCard";
+import styles from "./styles.module.css";
 
 export default function ConferencePageLayout() {
   return (
-    <div>
+    <div className={styles.conferencePageLayout}>
       <EventHeader title="컨퍼런스 · 세미나" count={10} />
+      <div className={styles.eventCardList}>
+        {[1, 2, 3, 4, 5].map((item) => (
+          <EventCard
+            key={item}
+            title="요즘 핫한 행사! 요즘 핫한 행사!"
+            date="2025.01.01 - 2025.01.01"
+            place="서울특별시 강남구 테헤란로 22길"
+            price="0원"
+            category="카테고리"
+          />
+        ))}
+      </div>
     </div>
   );
 }
