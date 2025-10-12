cfb3fb0c4bc9c1d49ffa0fb13e42dd46c6f700cd
gracekim527 <k07173027@gmail.com>
2025-10-12 15:59:33 +0900

    :sparkles: Feat: 컨퍼런스 내 필터 뷰 구현 (#4)

--- src/components/events/filters/views/ConferenceFilterView.tsx
+++ src/components/events/filters/views/ConferenceFilterView.tsx
@@ -4,15 +4,18 @@
 
 import { useState } from "react";
 import OnOfflineFilter from "../filterElements/OnOfflineFilter";
+import FreeFilter from "../filterElements/FreeFilter";
+import styles from "./styles.module.css";
 
 export default function ConferenceFilterView() {
   const [onOfflineFilter, setOnOfflineFilter] = useState<string>("");
   return (
-    <div>
+    <div className={styles.conferenceFilterView}>
       <OnOfflineFilter
         onSelect={setOnOfflineFilter}
         selected={onOfflineFilter}
       />
+      <FreeFilter />
     </div>
   );
 }
